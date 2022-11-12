d9e7e376c47f38de252aba59585e32880cefc77b
jakupemini5 <jakup.emini.5@gmail.com>
2022-11-12 15:53:25 +0100

    added appsettings configuration, database and databaseSeeder, together with entities

---
+++ DiansProject/DiansProject.DAL/Data/DatabaseSeeder.cs
@@ -0,0 +1,49 @@
+﻿using DiansProject.DAL.Entities;
+using DiansProject.DAL.Json_Models;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiansProject.DAL.Data
+{
+    public class DatabaseSeeder
+    {
+        public static void Seed(IServiceProvider applicationServices)
+        {
+            using (IServiceScope serviceScope = applicationServices.CreateScope())
+            {
+                DatabaseContext context = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
+                if (context.Database.EnsureCreated())
+                {
+                    //SeedFromRawJsonData();
+                }
+                SeedFromRawJsonData();
+            }
+        }
+
+        private static void SeedFromRawJsonData()
+        {
+            string rawDataFilePath = @"C:\Users\ThinkBook\source\repos\DiansProject\DiansProject\DiansProject.DAL\Raw Data\RawPointsData.txt";
+            string rawDataContent = "";
+
+
+            rawDataContent = File.ReadAllText(rawDataFilePath);
+
+
+            var openStreetData = JsonConvert.DeserializeObject<OpenStreetData>(rawDataContent);
+
+            foreach(var feature in openStreetData.Features)
+            {
+                var 
+            }
+
+
+
+            //context.SaveChanges();
+        }
+    }
+}
--- DiansProject/DiansProject/Program.cs
+++ DiansProject/DiansProject/Program.cs
@@ -1,10 +1,29 @@
+using DiansProject.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+IWebHostEnvironment environment = builder.Environment;
+
+var adsff = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+var Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", true,
+                    true)
+                .AddCommandLine(args)
+                .AddEnvironmentVariables()
+                .Build();
+
 // Add services to the container.
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(Configuration["ConnectionStrings"]));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+DatabaseSeeder.Seed(app.Services);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
