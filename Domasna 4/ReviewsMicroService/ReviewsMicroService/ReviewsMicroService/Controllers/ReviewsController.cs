c3175ae3a954a5105f68cc77da975e610ede3884
jakupemini5 <jakup.emini.5@gmail.com>
2023-01-29 15:55:26 +0100

    Added reviews controller and dependencies to database

--- Domasna 4/ReviewsMicroService/ReviewsMicroService/ReviewsMicroService/Controllers/ReviewsController.cs
+++ Domasna 4/ReviewsMicroService/ReviewsMicroService/ReviewsMicroService/Controllers/ReviewsController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ReviewsMicroService.BLL.Services.Interfaces;
 
 namespace ReviewsMicroService.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class ReviewsController : ControllerBase
     {
+        private readonly IReviewService _reviewService;
+
+        public ReviewsController(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+        [HttpGet("{featureId}")]
         public async Task<IActionResult> GetReviews(string featureId)
         {
-
-            return Ok();
+            return Ok(await _reviewService.GetFeatureReviews(featureId));
         }
     }
 }
--- Domasna 4/ReviewsMicroService/ReviewsMicroService/ReviewsMicroService/Program.cs
+++ Domasna 4/ReviewsMicroService/ReviewsMicroService/ReviewsMicroService/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using ReviewsMicroService.BLL.Services.Implementations;
+using ReviewsMicroService.BLL.Services.Interfaces;
+using ReviewsMicroService.DAL.Data;
+using ReviewsMicroService.DAL.Repositories.Implementations;
+using ReviewsMicroService.DAL.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", true,
+                    true)
+                .AddCommandLine(args)
+                .AddEnvironmentVariables()
+                .Build();
+
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(Configuration["ConnectionStrings"]));
+
+//repositories
+builder.Services.AddTransient<IReviewRepository, ReviewRepository>();
+//services
+builder.Services.AddTransient<IReviewService, ReviewService>();
 
 var app = builder.Build();
 
